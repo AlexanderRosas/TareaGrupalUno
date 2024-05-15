138e8adfb3635fda8dc6afb17ccb49bbd92d5437
RedJonny <jonathan-sacoto@hotmail.com>
2024-05-14 19:06:29 -0500

    DoWhile actualizacion

---
+++ Deber_Do_While/Deber_Do_while/src/imprimirJ.java
@@ -0,0 +1,12 @@
+public class imprimirJ {
+    public void imprimirJDoWhile(){
+        int contador = 0;
+
+        do {
+            System.out.println("J");
+            contador ++;
+            
+        } while (contador == 10);
+    }
+
+}
